af71ec1f063b0da68e5f8fc2d84c898a0cb76fd1
Jan Dlabal <admin@houbysoft.com>
2011-02-01 20:47:27 -0500

    Added shortcut (Ctrl+Q) to close window in the GUI

--- qtgui/hcgbasewindow.cpp
+++ qtgui/hcgbasewindow.cpp
@@ -51,6 +51,7 @@ HCGBaseWindow::HCGBaseWindow() : QMainWindow() {
   connect(hcgcore, SIGNAL(closeAll_signal()), this, SLOT(close()));
   createShortcut("Ctrl+O", this, SLOT(openScript()));
   createShortcut("Ctrl+N", this, SLOT(newScript()));
+  createShortcut("Ctrl+Q", this, SLOT(close()));
 
   log_edit = NULL;
 }
